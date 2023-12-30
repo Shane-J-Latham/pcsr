356d478b0a95d3cf3edc3e8f796ea3d2464e2b13
Shane-J-Latham <Shane-J-Latham@users.noreply.github.com>
2023-12-31 00:40:41 +1100

    Use setuptools.find_packages to discover packages and sub-packages.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from skbuild import setup
+from setuptools import find_packages
 import versioneer
 
 setup(
@@ -6,7 +7,7 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
-    packages=["pcsr",],
+    packages=find_packages(),
     package_data={'pcsr': ["models/data/*.obj"]},
     cmake_with_sdist=True
 )
